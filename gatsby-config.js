bc4945f3bdf764ff5e05199e85e0852cf9833166
Andre <andre.webderking@gmail.com>
2020-09-28 22:01:15 -0300

    Include gatsby-source-contenful plugin before mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,14 @@ module.exports = {
       },
 		},
 		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: `9pq5a30g28aj`,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+			},
+		},
+		{
 			resolve: `gatsby-plugin-mdx`,
 			options: {
 				defaultLayouts: {
@@ -65,14 +73,6 @@ module.exports = {
 				}
 			}
 		},
-		{
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `9pq5a30g28aj`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
