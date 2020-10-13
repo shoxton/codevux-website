fed5bec3426541d2ba9f074832d234bfd7e2b9f2
Andre <andre.webderking@gmail.com>
2020-10-12 22:39:47 -0300

    Add menus/submenus to mobile nav

--- src/components/navMobile.js
+++ src/components/navMobile.js
@@ -6,9 +6,14 @@ import {
 	DrawerHeader,
 	DrawerBody,
 	DrawerCloseButton,
-	Heading
+	Heading,
+	Box,
+	Stack,
+	Button,
+	Icon
  } from '@chakra-ui/core'
-import { NavMenu } from './nav'
+import { NavBar } from './nav'
+import Link from '../gatsby-plugin-chakra-ui/components/link';
 
 const NavMobile = (props) => {
 
@@ -21,7 +26,41 @@ const NavMobile = (props) => {
 					<Heading as="h4" size="md">CODEVUX</Heading>
 				</DrawerHeader>
 				<DrawerBody>
-					<NavMenu></NavMenu>
+				<Box
+					color="gray.700"
+				>
+					<Box pb={4}>
+						<Link fontWeight="bold" to="/desenvolvimento-web">Desenvolvimento Web <Icon name="arrow-forward" /> </Link>
+						<Stack borderLeft="1px" borderLeftColor="gray.300" pl={4} py={4}>
+							<Link to="/desenvolvimento-web/criar-site-institucional">Site institucional <Icon name="arrow-forward" /> </Link>
+							<Link to="/desenvolvimento-web/criar-loja-virtual">Loja virtual <Icon name="arrow-forward" /> </Link>
+							<Link to="/desenvolvimento-web/criar-blog">Blog <Icon name="arrow-forward" /> </Link>
+						</Stack>
+					</Box>
+					<Box pb={4}>
+						<Link fontWeight="bold" to="/integracao-de-sistemas">Integração de sistemas <Icon name="arrow-forward" /> </Link>
+						<Stack borderLeft="1px" borderLeftColor="gray.300" pl={4} py={4}>
+							<Link to="/integracao-de-sistemas/integrar-marketplaces">Marketplaces <Icon name="arrow-forward" /> </Link>
+							<Link to="/integracao-de-sistemas/integrar-meios-de-pagamento">Meios de pagamento <Icon name="arrow-forward" /> </Link>
+							<Link to="/integracao-de-sistemas/integrar-transportadoras">Transportadoras <Icon name="arrow-forward" /> </Link>
+						</Stack>
+					</Box>
+					<Box pb={4}>
+						<Link fontWeight="bold" to="/suporte">Suporte e consultoria <Icon name="arrow-forward" /> </Link>
+					</Box>
+					<Box pb={4}>
+						<Link fontWeight="bold" to="/magento">Soluções em Magento <Icon name="arrow-forward" /> </Link>
+					</Box>
+				</Box>
+				<Button
+					to="/contato"
+					as={Link}
+					variantColor="teal"
+					size="sm"
+					_hover={{textDecoration: 'none', ...Button._hover}}
+				>
+					Fale conosco
+				</Button>
 				</DrawerBody>
 			</DrawerContent>
 		</Drawer>
