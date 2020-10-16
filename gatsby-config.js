719db55a312eb761ad433d25562053add1251872
Andre <andre.webderking@gmail.com>
2020-10-15 23:44:58 -0300

    Config gatsby-source-wordpress-experimental options

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 require("dotenv").config({
+  path: `.env.GATSBY_CONCURRENT_DOWNLOAD`,
+})
+
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
@@ -64,7 +68,32 @@ module.exports = {
 				// Learn about environment variables: https://gatsby.dev/env-vars
 				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 			},
-		}
+		},
+    {
+      resolve: `gatsby-source-wordpress-experimental`,
+      options: {
+        url: process.env.WPGRAPHQL_URL,
+        verbose: true,
+        develop: {
+          hardCacheMediaFiles: true,
+        },
+        debug: {
+          graphql: {
+            writeQueriesToDisk: true,
+          },
+        },
+        type: {
+          Post: {
+            limit:
+              process.env.NODE_ENV === `development`
+                ? // Lets just pull 50 posts in development to make it easy on ourselves.
+                  50
+                : // and we don't actually need more than 5000 in production for this particular site
+                  5000,
+          },
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
