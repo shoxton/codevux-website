02fb232773cd5c0765f073172359748e1c67705e
Andre <andre.webderking@gmail.com>
2020-11-09 18:14:34 -0300

    add new product page

---
+++ src/pages/suporte/consultoria.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { graphql } from "gatsby"
+import ProductPage from "../../templates/productPage"
+
+export default (props) => (
+	<ProductPage {...props} />
+)
+
+
+
+
+export const query = graphql`
+	{
+		contentfulProduct (slug: {eq: "consultoria"})  {
+			title
+			slug
+			metaTitle
+			perks
+			headline
+			ctaText
+			description {
+				description
+			}
+			heroImage {
+				fluid(quality: 80, maxHeight: 300) {
+					...GatsbyContentfulFluid_withWebp_noBase64
+				}
+				title
+			}
+			features {
+				title
+				description
+			}
+			service {
+				slug
+				products {
+					title
+					slug
+					shortDescription
+				}
+			}
+			modules {
+				title
+				label
+				media {
+					fluid(quality: 80, maxHeight: 300) {
+						...GatsbyContentfulFluid_withWebp_noBase64
+					}
+					title
+				}
+				headline
+			}
+		}
+	}
+`
